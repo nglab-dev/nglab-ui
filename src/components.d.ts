c2e60126f56a5facb5971061f4a633bacf41ba6a
xbmlz <997909544@qq.com>
2024-06-28 10:29:39 +0800

    feat: add nginx conf

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,6 +7,10 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    Button: typeof import('primevue/button')['default']
+    Checkbox: typeof import('primevue/checkbox')['default']
+    InputText: typeof import('primevue/inputtext')['default']
+    Password: typeof import('primevue/password')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
