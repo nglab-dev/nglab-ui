21c083a1d4e1e8a7eff45984f92460810bd83d06
xbmlz <997909544@qq.com>
2024-08-15 09:42:49 +0800

    feat: add AppearanceTransition

--- src/components/DarkSwitcher/index.vue
+++ src/components/DarkSwitcher/index.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <button type="button" aria-label="dark" class="icon-btn" @click="toggleDark()">
+  <button type="button" aria-label="dark" class="icon-btn" @click="toggleDark">
     <div class="i-mdi-weather-night dark:i-mdi-weather-sunny" />
   </button>
 </template>
